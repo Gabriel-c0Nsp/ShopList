4de1b4fc747d5347957f19256d9bb43afe8ef607
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2024-08-09 21:45:14 -0300

    Reorganização de diretórios dentro do projeto

--- app/src/main/java/com/example/shoplist/MainActivity.kt
+++ app/src/main/java/com/example/shoplist/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.shoplist.MainScreen.MainScreen
 import com.example.shoplist.ui.theme.ShopListTheme
 
 class MainActivity : ComponentActivity() {
---
+++ app/src/main/java/com/example/shoplist/MainScreen/ListSection.kt
@@ -0,0 +1,9 @@
+package com.example.shoplist.MainScreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ListSection(modifier: Modifier = Modifier) {
+    // TODO: Implementar a lista para que possa ser personalizada sua ui aqui
+}
