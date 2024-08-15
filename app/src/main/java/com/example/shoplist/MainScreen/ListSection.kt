511f8487df7f77671e4af230d78216a1fa6296a9
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2024-08-14 23:52:23 -0300

    Adição de botões para editar e deletar lista (sem funcionalidade)

--- app/src/main/java/com/example/shoplist/MainScreen/ListSection.kt
+++ app/src/main/java/com/example/shoplist/MainScreen/ListSection.kt
@@ -9,14 +9,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.shoplist.R
 
 @Composable
 fun ListSection(modifier: Modifier = Modifier) {
@@ -39,20 +45,38 @@ fun ListSection(modifier: Modifier = Modifier) {
 
 @Composable
 fun ItemListName(modifier: Modifier = Modifier, name: String) {
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
             .clip(RoundedCornerShape(12.dp))
             .background(Color(0xFF0076D5))
-            .padding(16.dp)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Column {
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
             Text(
                 text = name,
                 color = Color.White,
                 fontSize = 18.sp,
                 fontWeight = FontWeight.Bold
+            )
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_mode_edit_outline_24),
+                contentDescription = "edit",
+                tint = Color.White
+            )
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_delete_24),
+                contentDescription = "delete",
+                tint = Color.White
             )
         }
     }
--- app/src/main/java/com/example/shoplist/MainScreen/ShoppingLists.kt
+++ app/src/main/java/com/example/shoplist/MainScreen/ShoppingLists.kt
@@ -13,9 +13,9 @@ fun testShoppingLists(): List<ShoppingLists> {
     return listOf(
         ShoppingLists(1, "Lista de compras Matheus", lists[0]),
         ShoppingLists(2, "Lista de compras para o fim de semana para a casa", lists[1]),
-        ShoppingLists(3, "Lista de frutas e vegetais", lists[2]),
-        ShoppingLists(4, "Lista de limpeza", lists[3]),
+        ShoppingLists(3, "Frutas e vegetais", lists[2]),
+        ShoppingLists(4, "Produtos de limpeza", lists[3]),
         ShoppingLists(5, "Lista de carnes", lists[4]),
-        ShoppingLists(6, "Lista de cuidados pessoais", lists[5])
+        ShoppingLists(6, "Lista de miojos e cup nuddles", lists[5])
     )
 }
